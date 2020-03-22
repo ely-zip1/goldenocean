b6dbfa6ebd57e7e1ab05a8d057d07d7fe69cd506
Elisha Lapiz <ely-zip1@users.noreply.github.com>
2020-03-23 00:22:36 +0800

    Update Your_deposits.php

--- application/controllers/Your_deposits.php
+++ application/controllers/Your_deposits.php
@@ -23,12 +23,53 @@ class Your_deposits extends CI_Controller
     $deposits = $this->DepositModel->get_deposit_per_member($member->id);
 
     $total_deposit = 0;
+    $ultramax_deposit_data = array();
+    $panamax_deposit_data = array();
+    $capesize_deposit_data = array();
+
     foreach ($deposits as $deposit) {
       if($deposit->is_pending == 0){
         $total_deposit += $deposit->amount;
+
+        if($deposit->package_id == 1){
+          $ultramax = array();
+          $ultramax['amount'] = number_format($deposit->amount, 2, '.', ',');
+
+          $payment_mode = $this->Deposit_Options->get_by_id($deposit->deposit_options_id);
+          $ultramax['mode'] = $payment_mode->name;
+          $ultramax['date'] = $deposit->date;
+          $ultramax['date_approved'] = $deposit->date_approved;
+
+          array_push($ultramax_deposit_data,$ultramax);
+        }
+        else if($deposit->package_id == 2){
+          $panamax = array();
+          $panamax['amount'] = number_format($deposit->amount, 2, '.', ',');
+
+          $payment_mode = $this->Deposit_Options->get_by_id($deposit->deposit_options_id);
+          $panamax['mode'] = $payment_mode->name;
+          $panamax['date'] = $deposit->date;
+          $panamax['date_approved'] = $deposit->date_approved;
+
+          array_push($panamax_deposit_data,$panamax);
+        }
+        else if($deposit->package_id == 2){
+          $capesize = array();
+          $capesize['amount'] = number_format($deposit->amount, 2, '.', ',');
+
+          $payment_mode = $this->Deposit_Options->get_by_id($deposit->deposit_options_id);
+          $capesize['mode'] = $payment_mode->name;
+          $capesize['date'] = $deposit->date;
+          $capesize['date_approved'] = $deposit->date_approved;
+
+          array_push($capesize_deposit_data, $capesize);
+        }
       }
     }
 
+    $data['ultramax_deposit_data'] = $ultramax_deposit_data;
+    $data['panamax_deposit_data'] = $panamax_deposit_data;
+    $data['capesize_deposit_data'] = $capesize_deposit_data;
     $data['total_details'] = '$ '. number_format($total_deposit, 2, '.', ',');
 
     $this->load->view('templates/header', $data);
