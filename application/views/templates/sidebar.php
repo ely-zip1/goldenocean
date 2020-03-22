e0724fea8e542a59172f86051672ac78092cec52
Elisha Lapiz <ely-zip1@users.noreply.github.com>
2020-03-22 21:59:46 +0800

    Update sidebar.php

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -21,19 +21,19 @@ defined('BASEPATH') or exit('No direct script access allowed');
 					href="account_settings" class="nav-link"><i class="fas fa-edit"></i><span>Edit Account</span></a>
 			</li>
 			<li class="dropdown <?php echo $this->uri->segment(1) == 'your_deposits' ? 'active' : ''; ?>"> <a
-					href="your_deposits" class="nav-link"><i class="fas fa-edit"></i><span>Your Deposits</span></a>
+					href="your_deposits" class="nav-link"><i class="fas fa-dollar-sign"></i><span>Your Deposits</span></a>
 			</li>
 			<li class="dropdown <?php echo $this->uri->segment(1) == 'withdraw' ? 'active' : ''; ?>"> <a
-					href="withdraw" class="nav-link"><i class="fas fa-edit"></i><span>Withdraw</span></a>
+					href="withdraw" class="nav-link"><i class="fas fa-money-check-alt"></i><span>Withdraw</span></a>
 			</li>
 			<li class="dropdown <?php echo $this->uri->segment(1) == 'earning_history' ? 'active' : ''; ?>"> <a
-					href="earning_history" class="nav-link"><i class="fas fa-edit"></i><span>Earning History</span></a>
+					href="earning_history" class="nav-link"><i class="fas fa-money-bill-wave-alt"></i><span>Earning History</span></a>
 			</li>
 			<li class="dropdown <?php echo $this->uri->segment(1) == 'referral_history' ? 'active' : ''; ?>"> <a
-					href="withdraw" class="nav-link"><i class="fas fa-edit"></i><span>Referrals History</span></a>
+					href="withdraw" class="nav-link"><i class="fas fa-user-clock"></i><span>Referrals History</span></a>
 			</li>
 			<li class="dropdown <?php echo $this->uri->segment(1) == 'withdrawal_history' ? 'active' : ''; ?>"> <a
-					href="withdrawal_history" class="nav-link"><i class="fas fa-edit"></i><span>Withdrawals History</span></a>
+					href="withdrawal_history" class="nav-link"><i class="fas fa-money-bill"></i><span>Withdrawals History</span></a>
 			</li>
 		</ul>
 	</aside>
