e85c2fc61409c990697eaa0ac258e095c9bfd585
Elisha Lapiz <ely-zip1@users.noreply.github.com>
2020-03-23 02:06:03 +0800

    Update withdraw.php

--- application/views/pages/withdraw.php
+++ application/views/pages/withdraw.php
@@ -59,7 +59,7 @@
           <div class="card bg-primary text-white">
               <div class="card-body">
                 <div class="row">
-          				<div class="col-md-9">
+          				<div class="col-md-12">
           					<?php echo form_open('withdrawal'); ?>
           					<div class="form-row">
           						<div class="form-group col-md-5 deposit-form">
@@ -87,7 +87,7 @@
                       </div>
 
                       <div class="form-group col-md-2">
-            	          <label for="deposit_amount" hidden>Submit</label>
+            	          <label for="deposit-submit-button">Submit</label>
               					<button type="submit" class="btn btn-block" name="deposit-submit-button">Withdraw</button>
               					<?php echo form_close(); ?>
               				</div>
