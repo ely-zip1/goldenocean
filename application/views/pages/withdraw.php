966a2f10f7bd81cfdca2362e5e7238632293c5f9
Elisha Lapiz <ely-zip1@users.noreply.github.com>
2020-03-23 01:53:02 +0800

    Update withdraw.php

--- application/views/pages/withdraw.php
+++ application/views/pages/withdraw.php
@@ -32,10 +32,10 @@
         <div class="col-12">
           <div class="card bg-info text-white">
             <div class="card-header">
-              <h4>Account Balance:  </h4>
+              <h1>Account Balance:  </h1>
             </div>
               <div class="card-body">
-                <h4>Pending Withdrawals:  </h4>
+                <h6>Pending Withdrawals:  </h6>
               </div>
           </div>
         </div>
@@ -54,41 +54,50 @@
         </div>
       </div>
 
-      <div class="row">
-				<div class="col-md-9">
-					<?php echo form_open('withdrawal'); ?>
-					<div class="form-row">
-						<div class="form-group col-md-6 deposit-form">
-							<label for="plan_option">Mode</label>
-							<select class="form-control" name="plan_payment_mode">
-								<option <?php if($selected_mode == 'mode1') {echo 'selected';} ?> value="mode1">Bitcoin</option>
-								<option <?php if($selected_mode == 'mode2') {echo 'selected';} ?> value="mode2">Abra</option>
-								<option <?php if($selected_mode == 'mode3') {echo 'selected';} ?> value="mode3">Neteller</option>
-								<option <?php if($selected_mode == 'mode4') {echo 'selected';} ?> value="mode4">Paypal</option>
-								<option <?php if($selected_mode == 'mode5') {echo 'selected';} ?> value="mode5">Mastercard</option>
-								<option <?php if($selected_mode == 'mode6') {echo 'selected';} ?> value="mode6">Skrill</option>
-							</select>
-							<div class="invalid-feedback">
-										<?php echo form_error('plan_payment_mode');?>
-							</div>
-            </div>
+      <div class="row deposit-row">
+        <div class="col-12">
+          <div class="card bg-primary text-white">
+              <div class="card-body">
+                <div class="row">
+          				<div class="col-md-9">
+          					<?php echo form_open('withdrawal'); ?>
+          					<div class="form-row">
+          						<div class="form-group col-md-6 deposit-form">
+          							<label for="plan_option">Mode</label>
+          							<select class="form-control" name="plan_payment_mode">
+          								<option <?php if($selected_mode == 'mode1') {echo 'selected';} ?> value="mode1">Bitcoin</option>
+          								<option <?php if($selected_mode == 'mode2') {echo 'selected';} ?> value="mode2">Abra</option>
+          								<option <?php if($selected_mode == 'mode3') {echo 'selected';} ?> value="mode3">Neteller</option>
+          								<option <?php if($selected_mode == 'mode4') {echo 'selected';} ?> value="mode4">Paypal</option>
+          								<option <?php if($selected_mode == 'mode5') {echo 'selected';} ?> value="mode5">Mastercard</option>
+          								<option <?php if($selected_mode == 'mode6') {echo 'selected';} ?> value="mode6">Skrill</option>
+          							</select>
+          							<div class="invalid-feedback">
+          										<?php echo form_error('plan_payment_mode');?>
+          							</div>
+                      </div>
 
-            <div class="form-group col-md-6 deposit-form">
-	            <label for="deposit_amount">Amount</label>
-	            <input type="text" class="form-control <?php if(strlen(form_error('deposit_amount')) > 0){echo "is-invalid";} ?>"
-								name="deposit_amount" id="deposit-amount" placeholder="Amount" value="<?php set_value('deposit_amount','',true); ?>">
-							<div class="invalid-feedback deposit-error">
-										<?php echo form_error('deposit_amount');?>
-							</div>
-            </div>
+                      <div class="form-group col-md-6 deposit-form">
+          	            <label for="deposit_amount">Amount</label>
+          	            <input type="text" class="form-control <?php if(strlen(form_error('deposit_amount')) > 0){echo "is-invalid";} ?>"
+          								name="deposit_amount" id="deposit-amount" placeholder="Amount" value="<?php set_value('deposit_amount','',true); ?>">
+          							<div class="invalid-feedback deposit-error">
+          										<?php echo form_error('deposit_amount');?>
+          							</div>
+                      </div>
+                    </div>
+          				</div>
+
+          				<div class="col-md-3">
+          					<button type="submit" class="rounded-button submit-deposit" name="deposit-submit-button">Withdraw</button>
+          					<?php echo form_close(); ?>
+          				</div>
+          			</div>
+              </div>
           </div>
-				</div>
+        </div>
+      </div>
 
-				<div class="col-md-3">
-					<button type="submit" class="rounded-button submit-deposit" name="deposit-submit-button">Withdraw</button>
-					<?php echo form_close(); ?>
-				</div>
-			</div>
 
     </div>
 	</section>
