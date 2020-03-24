b70a1f2cee784c6c7eedc39a895a79b6aaecc0ee
Elisha Lapiz <ely-zip1@users.noreply.github.com>
2020-03-24 15:57:27 +0800

    Update Upload.php

--- application/controllers/Upload.php
+++ application/controllers/Upload.php
@@ -27,13 +27,13 @@ class Upload extends CI_Controller {
                 {
                         $error = array('error' => $this->upload->display_errors());
 
-                        $this->load->view('upload_form', $error);
+                        $this->load->view('pages/upload_form', $error);
                 }
                 else
                 {
                         $data = array('upload_data' => $this->upload->data());
 
-                        $this->load->view('upload_success', $data);
+                        $this->load->view('pages/upload_success', $data);
                 }
         }
 }
