6e784b7df855a1ae4adc172ebf9d23e3f4b37997
Elisha Lapiz <ely-zip1@users.noreply.github.com>
2020-03-25 00:07:14 +0800

    Update header.php

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -11,6 +11,13 @@ if(!isset($this->session->username)){
 <html lang="en">
 
 <head>
+  <?php
+    header("Expires: Tue, 01 Jan 2000 00:00:00 GMT");
+    header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+    header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+    header("Cache-Control: post-check=0, pre-check=0", false);
+    header("Pragma: no-cache");
+  ?>
   <meta charset="UTF-8">
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
   <title><?php echo $title; ?> &mdash; Golden Ocean</title>
