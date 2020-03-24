704802ba88195ecc002d39cf2bbc4f13919282bc
Elisha Lapiz <ely-zip1@users.noreply.github.com>
2020-03-24 20:17:12 +0800

    Update sidebar.php

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -12,19 +12,19 @@ defined('BASEPATH') or exit('No direct script access allowed');
 		<ul class="sidebar-menu">
 			<!-- <li class="menu-header">Dashboard</li> -->
 			<li class="dropdown <?php echo $this->uri->segment(1) == 'dashboard' ? 'active' : ''; ?>"> <a
-					href="dashboard" class="nav-link"><i class="fas fa-home"></i><span>Dashboard</span></a>
+					href="<?= base_url();?>dashboard" class="nav-link"><i class="fas fa-home"></i><span>Dashboard</span></a>
 			</li>
 			<li class="dropdown <?php echo $this->uri->segment(1) == 'plans' ? 'active' : ''; ?>"> <a
-					href="plans" class="nav-link"><i class="fas fa-piggy-bank"></i><span>Make Deposit</span></a>
+					href="<?= base_url();?>plans" class="nav-link"><i class="fas fa-piggy-bank"></i><span>Make Deposit</span></a>
 			</li>
 			<li class="dropdown <?php echo $this->uri->segment(1) == 'account_settings' ? 'active' : ''; ?>"> <a
-					href="account_settings" class="nav-link"><i class="fas fa-edit"></i><span>Edit Account</span></a>
+					href="<?= base_url();?>account_settings" class="nav-link"><i class="fas fa-edit"></i><span>Edit Account</span></a>
 			</li>
 			<li class="dropdown <?php echo $this->uri->segment(1) == 'your_deposits' ? 'active' : ''; ?>"> <a
-					href="your_deposits" class="nav-link"><i class="fas fa-dollar-sign"></i><span>Your Deposits</span></a>
+					href="<?= base_url();?>your_deposits" class="nav-link"><i class="fas fa-dollar-sign"></i><span>Your Deposits</span></a>
 			</li>
 			<li class="dropdown <?php echo $this->uri->segment(1) == 'withdraw' ? 'active' : ''; ?>"> <a
-					href="withdraw" class="nav-link"><i class="fas fa-money-check-alt"></i><span>Withdraw</span></a>
+					href="<?= base_url();?>withdraw" class="nav-link"><i class="fas fa-money-check-alt"></i><span>Withdraw</span></a>
 			</li>
 			<!-- <li class="dropdown <?php echo $this->uri->segment(1) == 'earning_history' ? 'active' : ''; ?>"> <a
 					href="earning_history" class="nav-link"><i class="fas fa-money-bill-wave-alt"></i><span>Earning History</span></a>
