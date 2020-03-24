e1710ee1fa563eed38f1ce89930313a52c49cecb
Elisha Lapiz <ely-zip1@users.noreply.github.com>
2020-03-24 17:22:37 +0800

    Update layout.php

--- application/views/templates/layout.php
+++ application/views/templates/layout.php
@@ -17,24 +17,3 @@ defined('BASEPATH') or exit('No direct script access allowed');
           <a href="<?php echo base_url('logout'); ?>" class="btn btn-icon icon-left rounded-button header-button-2"><i class="fas fa-sign-out-alt"></i> LOGOUT</a>
         </div>
       </nav>
-      <div class="section-body">
-        <div class="row ">
-          <div class="col-12 user-basic-info">
-            <div class="row">
-              <div class="col-md-2 text-center">
-                <img src="<?php echo base_url('assets/img/user-logo.png'); ?>" alt="" class="user-img">
-              </div>
-              <div class="col-md-7">
-                <h4>WELCOME BACK, <?php echo strtoupper($this->session->userdata('username')); ?></h4>
-                <p>
-                  Signup Date: <span class="bold-text"> <?php echo strtoupper($this->session->userdata('date_registered')); ?></span><br>
-                  <!-- Last Access: <?php echo strtoupper($this->session->userdata('username')); ?> -->
-                </p>
-              </div>
-              <div class="col-md-3 text-center">
-                <a href="<?php echo base_url('account_settings'); ?>" class="btn btn-icon icon-left btn-dark rounded-button rounded-button-settings"><i class="fas fa-cogs"></i> Settings</a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
