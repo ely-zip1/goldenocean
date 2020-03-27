e9d25a8cecd4f1c46ccbeac528b28e2abab71fd0
Elisha Lapiz <ely-zip1@users.noreply.github.com>
2020-03-27 08:36:06 +0800

    Update Fund_transfer.php

--- application/controllers/Fund_transfer.php
+++ application/controllers/Fund_transfer.php
@@ -37,7 +37,7 @@ class Fund_transfer  extends CI_Controller
 
     $data['account_balance'] = $account_balance;
 
-    $this->form_validation->set_rules('receiver_code', 'Code', 'required|callback_validate_transfer');
+    $this->form_validation->set_rules('receiver_code', 'Code', 'required|callback_validate_code');
     $this->form_validation->set_rules('username', 'Username', 'required|regex_match[/^(\d*\.)?\d+$/]|less_than_equal_to['.$account_balance.']');
 
     if($this->form_validation->run() == FALSE){
